dcde80f4a22ddfc5549a17d3253a023818658547
Fabian Beuke <mail@beuke.org>
2020-09-18 10:40:33 +0200

    add redux devtool extension

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,11 @@ import { combineReducers } from 'redux'
 import textData from './text'
 import typingData from './typing'
 
+
+const d = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
 export default combineReducers({
     textData,
     typingData,
+    d,
 })
